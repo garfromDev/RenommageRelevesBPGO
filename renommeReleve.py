31670ce6ec4a8b4152e943a8ea3d1179028917cf
garfromDev <garfromdev@gmail.com>
2020-03-08 14:49:31 +0100

    fix import sys

--- renommeReleve.py
+++ renommeReleve.py
@@ -8,6 +8,7 @@
 
 import os as os
 import re
+import sys
 
 
 def main(path):
@@ -23,8 +24,10 @@ def main(path):
     new_name = u"%s%s.pdf" % (prefix, cb)
     print "renomme %s en %s" % (f, new_name)
     os.rename(path, os.path.join(os.path.dirname(path), new_name))
+    return os.path.join(os.path.dirname(path), new_name)
 
 
 if __name__ == '__main__':
-    if len(argv) >= 1:
-        main(argv[1])
+    if len(sys.argv) >= 1:
+        print("handling %s" % sys.argv[1])
+        main(sys.argv[1])
